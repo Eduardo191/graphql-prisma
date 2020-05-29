3bd61826f6c8ff90083cd8dbb5b4d2260c6a82c0
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-05-29 14:40:54 -0300

    Criando funções de criação e atualização de posts

--- src/prisma.js
+++ src/prisma.js
@@ -4,3 +4,38 @@ const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: 'http://192.168.99.100:4466/'
 })
+
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost({
+    where: {
+      id: postId
+    },
+    data
+  }, '{ author { id } }')
+  const user = await prisma.query.user({
+    where: {
+      id: post.author.id
+    }
+  }, '{ id name email posts { id title published } }')
+  return user
+}
+
+const createPostForUser = async (authorId, data) => {
+  const post = await prisma.mutation.createPost({
+    data: {
+      ...data,
+      author: {
+        connect: {
+          id: authorId
+        }
+      }
+    }
+  }, '{ id }')
+  const user = await prisma.query.user({
+    where: {
+      id: authorId
+    }
+  }, '{ id name email posts { id title published } }')
+  return user
+}
+
