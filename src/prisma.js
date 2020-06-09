8410c2b9844d723cc3fbfddc94e9a1e5e78b43ea
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-06-09 17:12:29 -0300

    Integrando banco de dados ao node com prisma

--- src/prisma.js
+++ src/prisma.js
@@ -5,40 +5,42 @@ const prisma = new Prisma({
   endpoint: 'http://192.168.99.100:4466/'
 })
 
-const updatePostForUser = async (postId, data) => {
-  const postExists = await prisma.exists.Post({ id: postId })
+export { prisma as default }
 
-  const post = await prisma.mutation.updatePost({
-    where: {
-      id: postId
-    },
-    data
-  }, '{ author { id name email posts { id title published } } }')
+// const updatePostForUser = async (postId, data) => {
+//   const postExists = await prisma.exists.Post({ id: postId })
+
+//   const post = await prisma.mutation.updatePost({
+//     where: {
+//       id: postId
+//     },
+//     data
+//   }, '{ author { id name email posts { id title published } } }')
   
-  return post.author
-}
+//   return post.author
+// }
 
-const createPostForUser = async (authorId, data) => {
-  const userExists = await prisma.exists.User({ id: authorId })
+// const createPostForUser = async (authorId, data) => {
+//   const userExists = await prisma.exists.User({ id: authorId })
 
-  if (!userExists) {
-    throw new Error('User not found!')
-  }
+//   if (!userExists) {
+//     throw new Error('User not found!')
+//   }
 
-  const post = await prisma.mutation.createPost({
-    data: {
-      ...data,
-      author: {
-        connect: {
-          id: authorId
-        }
-      }
-    }
-  }, '{ author { id name email posts { id title published } } }')
+//   const post = await prisma.mutation.createPost({
+//     data: {
+//       ...data,
+//       author: {
+//         connect: {
+//           id: authorId
+//         }
+//       }
+//     }
+//   }, '{ author { id name email posts { id title published } } }')
   
-  return post.author
-}
+//   return post.author
+// }
 
-updatePostForUser('ckar37aoq004307053frywkk1', {
-  body: 'Body updated'
-}).then((user) => console.log(JSON.stringify(user))).catch((err) => console.log(err))
+// updatePostForUser('ckar37aoq004307053frywkk1', {
+//   body: 'Body updated'
+// }).then((user) => console.log(JSON.stringify(user))).catch((err) => console.log(err))
