ce9e20bfbd6a71068ca03bea11d3774e27c8c035
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-06-15 19:50:32 -0300

    Adicionando autenticação as queries me e post

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId'
+
 const Query = {
     users(parent, args, { prisma }, info) {
       const opArgs = {}
@@ -32,21 +34,36 @@ const Query = {
 
       return prisma.query.posts(null, info)
     },
-    me() {
-      return {
-        id: '123908',
-        name: 'Mike',
-        email: 'mike@gmail.com',
-        age: 27
-      }
+    me(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request)
+
+      return prisma.query.user({
+        where: {
+          id: userId
+        }
+      })
     },
-    post() {
-      return {
-        id: '123098',
-        title: 'My test post',
-        body: 'This is my first post',
-        published: true
+    async post(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request, false)
+
+      const posts = await prisma.query.posts({
+        where: {
+          id: args.id,
+          OR: [{
+            published: true
+          }, {
+            author: {
+              id: userId
+            }
+          }]
+        }
+      }, info)
+
+      if (posts.length === 0) {
+        throw new Error('Post not found')
       }
+
+      return posts[0]
     }
 }
 
