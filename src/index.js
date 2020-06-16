32db093a6c1ebce4d9f6079aecbba7f1448c1747
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-06-16 17:20:28 -0300

    Utilizando fragments

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
-import {
-  GraphQLServer,
-  PubSub
-} from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
-import Subscription from './resolvers/Subscription'
 import prisma from './prisma'
+import { resolvers, fragmentReplacements } from './resolvers/index'
 
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    Post,
-    User,
-    Comment
-  },
+  resolvers,
   context(request) {
     return {
       db,
@@ -30,15 +15,14 @@ const server = new GraphQLServer({
       prisma,
       request
     }
-  }
+  },
+  fragmentReplacements
 })
 
 const options = {
   port: 3000
 }
 
-server.start(options, ({
-  port
-}) => {
+server.start(options, ({ port }) => {
   console.log(`The server is up on port ${port}`)
 })
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,13 +1,29 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
-  email(parent, args, { request }, info) {
-    const userId = getUserId(request, false)
+  email: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { request }, info) {
+      const userId = getUserId(request, false)
 
-    if (userId && userId === parent.id) {
-      return parent.email
-    } else {
-      return null
+      if (userId && userId === parent.id) {
+        return parent.email
+      } else {
+        return null
+      }
+    }
+  },
+  posts: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { prisma }, info) {
+      return prisma.query.posts({
+        where: {
+          author: {
+            id: parent.id
+          },
+          published: true
+        }
+      })
     }
   }
 }
