edb482e2dac380a591b83493bc8916e399f16991
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-07-06 15:58:44 -0300

    Reiniciando banco de dados de teste antes da cada teste

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,50 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost, { gql } from 'apollo-boost'
 import prisma from '../src/prisma'
+import bcrypt from 'bcryptjs'
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000'
+})
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts()
+  await prisma.mutation.deleteManyUsers()
+
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: 'Jhon',
+      email: 'jhon@gmail.com',
+      password: bcrypt.hashSync('jhon123@!')
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'My published post',
+      body: '',
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'My draft post',
+      body: '',
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+
 })
 
 test('Should create a new user', async () => {
