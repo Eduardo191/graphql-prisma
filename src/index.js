80bba68505e02fae15eff900b6285a445277620d
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-07-06 14:49:58 -0300

    Configurando ambiente para testes de queries e mutations

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import '@babel/polyfill'
-import { GraphQLServer, PubSub } from 'graphql-yoga'
-import db from './db'
-import prisma from './prisma'
-import { resolvers, fragmentReplacements } from './resolvers/index'
+import server from './server'
 
-const pubsub = new PubSub()
-
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request
-    }
-  },
-  fragmentReplacements
-})
-
-const options = {
-  port: process.env.PORT || 3000
-}
-
-server.start(options, ({ port }) => {
+server.start({ port: process.env.PORT || 3000 }, ({ port }) => {
   console.log(`The server is up on port ${port}`)
 })
