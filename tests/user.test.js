e3106177177fb716094cccff9019255533a40f24
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-07-08 14:26:55 -0300

    Utilizando variáveis em graphql

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,34 +1,25 @@
 import 'cross-fetch/polyfill'
-import { gql } from 'apollo-boost'
 import prisma from '../src/prisma'
 import seedDatabase, { userOne } from './utils/seedDatabase'
 import getClient from './utils/getClient'
+import { createUser, getProfile, getUsers, login } from './utils/operations'
 
 const client = getClient()
 
 beforeEach(seedDatabase)
 
 test('Should create a new user', async () => {
-  const createUser = gql`
-    mutation {
-      createUser(
-        data: {
-          name: "Eduardo",
-          email: "edu@hotmail.com",
-          password: "edu123"
-        }
-      ) {
-        token
-        user {
-          id 
-          name
-          email
-        }
-      }
+  const variables = {
+    data: {
+      name: 'Eduardo',
+      email: 'edu@exemplo.com',
+      password: 'mypass123'
     }
-  `
+  }
+
   const response = await client.mutate({
-    mutation: createUser
+    mutation: createUser,
+    variables
   })
 
   const exists = await prisma.exists.User({ id: response.data.createUser.user.id })
@@ -36,15 +27,6 @@ test('Should create a new user', async () => {
 })
 
 test('Should expose public author profile', async () => {
-  const getUsers = gql`
-    query {
-      users {
-        id
-        name
-        email
-      }
-    }
-  `
   const response = await client.query({ query: getUsers })
 
   expect(response.data.users.length).toBe(1)
@@ -53,51 +35,30 @@ test('Should expose public author profile', async () => {
 })
 
 test('Should not login with bad crdentials', async () => {
-  const login = gql`
-    mutation {
-      login(
-        data: {
-          email: "maria@gmail.com",
-          password: "ahfouqdoy8"
-        }
-      ) {
-        token
-      }
+  const variables = {
+    data: {
+      email: 'maria@gmail.com',
+      password: 'ahfouqdoy8'
     }
-  `
+  }
 
-  await expect(client.mutate({ mutation: login })).rejects.toThrow()
+  await expect(client.mutate({ mutation: login, variables })).rejects.toThrow()
 })
 
 test('Should not sign up with short password', async () => {
-  const createUser = gql`
-    mutation {
-      createUser(
-        data: {
-          name: "Pedro",
-          email: "pedro@bol.com.br",
-          password: "p"
-        }
-      ) {
-        token
-      }
+  const variables = {
+    data: {
+      name: 'Pedro',
+      email: 'pedro@bol.com.br',
+      password: 'p'
     }
-  `
-  await expect(client.mutate({ mutation: createUser })).rejects.toThrow()
+  }
+
+  await expect(client.mutate({ mutation: createUser, variables })).rejects.toThrow()
 })
 
 test('Should fetch user profile', async () => {
   const client = getClient(userOne.jwt)
-
-  const getProfile = gql`
-    query {
-      me {
-        id
-        name
-        email
-      }
-    }
-  `
   const { data } = await client.query({ query: getProfile })
   
   expect(data.me.id).toBe(userOne.user.id)
